8e2831f85a4f972b3a34bbbbf6d1b0d68bed82ff
Ankit <ankitsharma.as420@gmail.com>
2025-04-18 15:21:22 +0530

    changes in sql column field from false to true.

--- OnlineJobPortal/src/main/java/com/example/OnlineJobPortal/Entity/Profile.java
+++ OnlineJobPortal/src/main/java/com/example/OnlineJobPortal/Entity/Profile.java
@@ -1,30 +1,36 @@
 package com.example.OnlineJobPortal.Entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
-@Table(name ="profiles")
+@Table(name = "profiles")
 public class Profile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String profileSummery;
-    @Column(length = 10, nullable = false)
+    @Column(length = 10, nullable = true)
     private String mobile;
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String address;
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String city;
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String state;
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String country;
-    @Column(nullable = false)
+    @Column(nullable = true)
     private int workExperience;
     private String currentCompony;
 
-//  one profile can have one user.
+    //  one profile can have one user.
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")
     private User user;
